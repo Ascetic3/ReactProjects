4a8d332cb395df548c2f675565319a4a0bf398cc
Ascetic3 <asisolop@gmail.com>
2024-02-03 10:33:02 +0500

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { Component } from "react";
+import TodoList from './TodoList';
 
-const date1 = new Date(2021, 7, 19, 14, 5);
-const date2 = new Date(2021, 7, 19, 15, 23);
+// const date1 = new Date(2021, 7, 19, 14, 5);
+// const date2 = new Date(2021, 7, 19, 15, 23);
+
+// const [date1String, date2String] = [date1.toLocaleString, date2.toLocaleString]
+
+const date1: object = new Date(2021, 7, 19, 14, 5);
+const date2: object = new Date(2021, 7, 19, 15, 23);
 
 const initialData = [
   {
@@ -9,15 +15,18 @@ const initialData = [
     desc: 'Да поскорее!',
     image: '',
     done: true,
-    createAt: date1.toLocaleStirng(),
-    key: date1.getTime()
+    // createAt: date1.toLocaleStirng(),
+    // createAt: date1String,
+    createAt: date1.toLocaleString,
+    key: date1.getTime(),
   },
   {
     title: 'Написать первое React-приложение', 
     desc: 'Список запланнированных дел',
     image: '',
     done: false,
-    createAt: date2.toLocaleStirng(),
+    // createAt: date2String,
+    createAt: date2.toLocaleString,
     key: date2.getTime()
   },
 ];
@@ -33,13 +42,13 @@ export default class App extends Component{
       <div>
         <nav className="navbar is-light">
           <div className="navbar-brand">
-            <span className="navbar-item is-uppercase">
-              Todos
-            </span>
+          <span className="navbar-item is-uppercase">
+            Todos
+          </span>
           </div>
         </nav>
         <main className="content px-6 mt-6">
-          <h1>Todos</h1>
+          <TodoList list={this.data} />
         </main>
       </div>
     );
